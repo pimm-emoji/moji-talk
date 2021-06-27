459944917afa67596fc5b49892a0ad265b015ff4
Park, Jonghyeon <belline0124@gmail.com>
2021-06-28 00:05:06 +0900

    Reorganize assets

---
+++ Assets/Prefabs/LevelFlow/Emote/Emoji/EmojiController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+    The EmojiController Class
+*/
+public class EmojiController : MonoBehaviour
+{
+    public Emoji EmojiData;
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
