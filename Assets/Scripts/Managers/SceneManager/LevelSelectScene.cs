dd4abc7eded09d11bff8f803948a6a62fcb165a9
ShapeLayer <belline0124@gmail.com>
2021-07-08 15:06:53 +0900

    Fix bug that chatting room image isn't displayed

--- Assets/Scripts/Managers/SceneManager/LevelSelectScene.cs
+++ Assets/Scripts/Managers/SceneManager/LevelSelectScene.cs
@@ -9,15 +9,13 @@ public class LevelSelectScene : MonoBehaviour
     public GameObject WrapperControllerPrefab;
     void Start()
     {
-        // string prefabPath = Path.Combine(Configs.PrefabPath, "LevelSelectScene", "SidebarContentWrapper", "SidebarContentWrapper.prefab");
-        // WrapperControllerPrefab = Resources.Load(prefabPath, typeof(GameObject)) as GameObject;
         levels = GameManager.instance.GetLevelsPreset();
         foreach (KeyValuePair<string, LevelData> items in levels)
         {
             GameObject newObject = Instantiate(WrapperControllerPrefab) as GameObject;
             newObject.transform.SetParent(GameObject.Find("Content").transform);
             newObject.GetComponent<RectTransform>().sizeDelta = new Vector2(800f, 200f);
-            newObject.GetComponent<WrapperController>().Init(items.Value.name, items.Value.desc, Path.Combine(Configs.ResourcePath, items.Value.img));
+            newObject.GetComponent<WrapperController>().Init(items.Value.name, items.Value.desc, items.Value.img);
         }
     }
 }
