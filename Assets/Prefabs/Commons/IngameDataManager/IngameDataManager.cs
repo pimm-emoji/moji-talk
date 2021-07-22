4fae8e4fa7f8eb9460c24144e55216ac3e36a926
Park, Jonghyeon <belline0124@gmail.com>
2021-07-22 20:52:26 +0900

    Add methods to IngameDataManager

--- Assets/Prefabs/Commons/IngameDataManager/IngameDataManager.cs
+++ Assets/Prefabs/Commons/IngameDataManager/IngameDataManager.cs
@@ -1,6 +1,8 @@
+using System.IO;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Newtonsoft.Json.Linq;
 
 /*
     The IngameDataManager Class
@@ -16,7 +18,7 @@ public class IngameDataManager : MonoBehaviour
     public string levelID;
     public LevelData levelData;
     public List<Profile> participants;
-    public Flow flow;
+    public List<Level> flow;
 
     void Start()
     {
@@ -27,18 +29,38 @@ public class IngameDataManager : MonoBehaviour
     }
 
     public void SetLevelID(string LevelID) { levelID = LevelID; }
-    public void LoadLevelID() {}
     public string GetLevelID() { return levelID; }
 
-    public void SetLevelFlow(Flow Flow) { flow = Flow; }
+    public void SetLevelFlow(List<Level> Flow) { flow = Flow; }
     public void LoadLevelFlow() { LoadLevelFlow(levelID); }
     public void LoadLevelFlow(string LevelID) 
+    {
+        flow = PresetController.LoadSingleDepth<Level>(
+            PresetController.LoadJsonToArray(Path.Combine(Configs.LevelDirPath, LevelID, "flow.json"))
+        );
+    }
+    public List<Level> GetLevelFlow() { return flow; }
+
+    public void SetParticipants(List<Profile> Participants) { participants = Participants; }
+    public void LoadParticipants(List<string> Participants)
+    { // 사용하는것만 로드시킬 수 있게 할 것
+        participants = new List<Profile>();
+        foreach (string Participant in Participants)
+        {
+            participants.Add(
+                JObject.Parse(File.ReadAllText(
+                    Path.Combine(Configs.PresetProfileDirPath, Participant)
+                ).ToObject<Profile>())
+            );
+        }
+    }
+    public List<Profile> GetParticipants() { return participants; }
+
+    public void LoadLevel(string LevelID)
+    {
+        levelID = LevelID;
+        LoadLevel();
+    }
+    public void LoadLevel()
     {}
-    public void GetLevelFlow() {}
-
-    public void SetParticipants() {}
-    public void LoadParticipants() {}
-    public void GetParticipants() {}
-
-    public void LoadLevel() {}
 }
