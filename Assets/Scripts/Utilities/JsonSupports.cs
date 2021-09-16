059145096fa1aa2c811b662b2ef5b225402372db
Park, Jonghyeon <belline0124@gmail.com>
2021-09-17 08:52:47 +0900

    Update JsonSupports.cs

--- Assets/Scripts/Utilities/JsonSupports.cs
+++ Assets/Scripts/Utilities/JsonSupports.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
@@ -9,4 +10,39 @@ class JsonIO
     {
         return JsonUtility.FromJson<T>(File.ReadAllText(path));
     }
+    public static T LoadJsonAssetToObject<T>(string path)
+    {
+        return JsonUtility.FromJson<T>(Resources.Load<TextAsset>(path).text);
+    }
 }
+
+/*
+[System.Serializable]
+public class DictionarySerializableObject<TKey, TValue> : ISerializationCallbackReceiver
+{
+    [SerializeField] List<TKey> keys;
+    [SerializeField] List <TValue> values;
+    
+    Dictionary<TKey, TValue> target;
+    public Dictionary<TKey, TValue> ToDictionary() { return target; }
+
+    public DictionarySerializableObject(Dictionary<TKey, TValue> Target)
+    {
+        target = Target;
+    }
+    public void OnBeforeSerialize() 
+    {
+        keys = new List<TKey>(target.Keys);
+        values = new List<TValue>(target.Values);
+    }
+    public void OnAfterDeserialize()
+    {
+        var count = Math.Min(keys.Count, values.Count);
+        target = new Dictionary<TKey, TValue>(count);
+        for (var i = 0; i < count; ++i)
+        {
+            target.Add(keys[i], values[i]);
+        }
+    }
+}
+*/
