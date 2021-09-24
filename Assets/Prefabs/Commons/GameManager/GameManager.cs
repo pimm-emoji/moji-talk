7e84f782cb6f4b3cdea80b9b21b0ecbdd1f04c00
Park, Jonghyeon <belline0124@gmail.com>
2021-09-24 14:33:40 +0900

    Revert "Add Method Updates EmojiGenerations Config"
    This reverts commit 7eb97650b513743b76db13154bff834c6594fa80.

--- Assets/Prefabs/Commons/GameManager/GameManager.cs
+++ Assets/Prefabs/Commons/GameManager/GameManager.cs
@@ -45,6 +45,7 @@ public class GameManager : MonoBehaviour
     // * SetScore()
     public float userTotalScore = 0f;
     public float branchIndexingScore = 0f;
+    public int nowFlowIndex = 1;
     
     void Awake()
     {
